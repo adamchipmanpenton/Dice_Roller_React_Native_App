726c50c2ea12dc9385eb8be7db428d1b44a54253
adamchipmanpenton <adamchipmanpenton@gmail.com>
2022-07-20 11:55:25 -0230

    Learning React Native

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput  } from 'react-native';
 
 export default function App() {
+
+  const [text, onChangeText] = React.useState("");
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>D20</Text>
+      <TextInput
+    
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      
+
+
     </View>
+
+
+
+
+
   );
 }
 
@@ -16,5 +36,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
